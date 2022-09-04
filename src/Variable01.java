f33b5605981cb8842a02f4719dfb5a12ada5a291
k9 <k9@localhost>
2022-09-04 14:29:52 +0900

    first commit

--- src/Variable01.java
+++ src/Variable01.java
@@ -6,7 +6,6 @@ public class Variable01 {
          String str = "ウェブ　キャンプ之助";
          System.out.println(str);
          str = "少年よ大志を抱け";
-         System.out.println(str);
          System.out.println(LUCKY_NUMBER);
     }
 }
