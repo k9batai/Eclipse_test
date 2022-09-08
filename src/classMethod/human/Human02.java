fd3668db5da2345ae253022e0db226bc8b6be5ba
k9 <k9batai33@gmail.com>
2022-09-08 09:36:24 +0900

    classとmethod

---
+++ src/classMethod/human/Human02.java
@@ -0,0 +1,16 @@
+package classMethod.human;
+
+public class Human02 {
+	public String name;
+	public int age;
+	
+	public Human02() {
+		name = "山田";
+		age = 20;
+	}
+	
+	public Human02(String name, int ago) {
+		this.name = name;
+		this.age = ago;
+	}
+}
