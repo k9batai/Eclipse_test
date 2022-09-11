e29a48a8d3f812317a5b0eb10b4821ac72fb8229
k9 <k9batai33@gmail.com>
2022-09-11 18:05:12 +0900

    定数練習問題

--- src/Continue01.java
+++ src/Continue01.java
@@ -1,11 +1,12 @@
+
 public class Continue01 {
-    public static void main(String[] args) {
-        for (int count = 0; count < 5; count++) {
-            if (count == 2) {
-                // countが2の場合、この後の処理をスキップ
-                continue;
-            }
-            System.out.println("Continue01 = " + count);
-        }
-    }
+	
+	public static void main(String[] args) {
+		String name = "kk";
+		System.out.println(name);
+		name = "kkk";
+		System.out.println(name);
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
 }
