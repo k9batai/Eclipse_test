196bc609865db3733b102a16fc327612e05cef17
k9 <k9batai33@gmail.com>
2022-09-11 21:34:57 +0900

    if文　練習

---
+++ src/Chapter07.java
@@ -0,0 +1,18 @@
+
+public class Chapter07 {
+	public static void main(String[] args) {
+		int num = 113;
+		if (num <= 5) {
+			System.out.println("とても近いです。");
+		}
+		else if (num <= 10) {
+			System.out.println("近いです。");
+		}
+		else if (num <= 15) {
+			System.out.println("遠いです。");
+		}
+		else {
+			System.out.println("とても遠いです。");
+		}
+	}
+}
