fcae542f57c89a22035e75820640d02b17d72ecd
k9 <k9batai33@gmail.com>
2022-09-13 22:58:42 +0900

    拡張for文

--- src/For02.java
+++ src/For02.java
@@ -2,7 +2,7 @@ public class For02 {
     public static void main(String[] args) {
         int[] array = { 1, 2, 3, 4, 5 };
         for (int number : array) {
-            System.out.println("For02 = " + number);
+            System.out.println(number);
         }
     }
 }
