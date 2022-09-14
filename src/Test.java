0cd9189f6343e073f2cb56415dbf7bf1aeebeab9
k9 <k9batai33@gmail.com>
2022-09-14 21:06:44 +0900

    入れ替え文

--- src/Test.java
+++ src/Test.java
@@ -2,11 +2,36 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
 		int x = 11;
 		System.out.println("x=" + x);
+		
 		x = 13;
 		int y = 17;
-		System.out.println("x=" + x);
-		System.out.println("y=" + y);
+		System.out.println("x=" + x + "y=" + y);
+		
+		x = 13 + 17;
+		System.out.println(x);
+		System.out.println(13*17);
+		
+		x = 7;
+		x *= 3;
+		System.out.println(x);
+		
+		x = 21 / 2;
+		System.out.println(x);
+		
+		int t;
+		t = x;
+		x = y;
+		y = t;
+		System.out.println("x=" + x + ",y=" + y);
+		
+		x = 19;
+		y = 23;
+		int z = x * y;
+		System.out.println(z);
+		
+		
 	}
 }
