e318359f8ea0a3668e72651b2b5f5c9632139a92
k9 <k9batai33@gmail.com>
2022-09-14 19:21:16 +0900

    表示、変数　練習問題

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int x = 11;
+		System.out.println("x=" + x);
+		x = 13;
+		int y = 17;
+		System.out.println("x=" + x);
+		System.out.println("y=" + y);
+	}
+}
