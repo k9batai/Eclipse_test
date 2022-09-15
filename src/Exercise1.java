20af00b9eb1d481f6bc6537a33621160d7647992
k9 <k9batai33@gmail.com>
2022-09-15 21:19:06 +0900

    入力　練習問題

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -20,5 +20,24 @@ public class Exercise1
         System.out.println( x );
         System.out.println( x * x );
         System.out.println( x * x * x );
+        
+        x = Integer.parseInt( br.readLine() );
+        int y = Integer.parseInt( br.readLine() );
+
+        System.out.println("和 " + ( x + y ) );
+        System.out.println("差 " + ( x - y ) );
+        System.out.println("積 " + ( x * y ) );
+        System.out.println("商 " + ( x / y ) );
+        System.out.println("余り " + ( x % y ) );
+        
+        x = Integer.parseInt( br.readLine() );
+        y = Integer.parseInt( br.readLine() );
+
+        System.out.println( "平均値=" + ( ( x + y ) / 2 ) );
+        
+        System.out.println( "あなたの年齢は？" );
+        int age = Integer.parseInt( br.readLine() );
+
+        System.out.println( "生まれてから今まで、およそ " + ( age * 365 ) + "日です" );
     }
 }
