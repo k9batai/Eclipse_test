0bfde33a3e73aca2600d96688b7f6156ec1bffab
k9 <k9batai33@gmail.com>
2022-09-15 20:42:56 +0900

    繰り返し文

---
+++ src/Exercise.java
@@ -0,0 +1,22 @@
+public class Exercise {
+    public static void main(String[] args) throws Exception {
+    	for( int i = 0 ; i < 10 ; i++ )
+    	    System.out.print( "SPAM" );
+    	
+    	for( int i = 1 ; i <= 9 ; i++ )
+    	    System.out.print( ( 3 * i ) + " " );
+    	
+    	int x = 1;
+
+    	for( int n = 1 ; n <= 8 ; n++ ) {
+    	    x *= 2;
+    	    System.out.println( "2の" + n + "乗=" + x );
+    	}
+    	
+    	x = 1;
+    	for( int i = 2 ; i <= 7 ; i++ )
+    	    x *= i;
+
+    	System.out.println( x );
+    }
+}
