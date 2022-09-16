6f8f368326e1ea2bd78a28c766e194b89b651428
k9 <k9batai33@gmail.com>
2022-09-16 23:33:45 +0900

    フィボナッチ数列

--- src/Exercise.java
+++ src/Exercise.java
@@ -28,5 +28,18 @@ public class Exercise {
 		    sum += Integer.parseInt( br.readLine() );
 
 		System.out.println( "合計は " + sum );
+		
+		int     n1 = 0;
+		int     n2 = 1;
+		int     n3 = n1 + n2;
+
+		while( n1 <= 1000 )
+		{
+		    System.out.print( n1 + "," );
+
+		    n1 = n2;
+		    n2 = n3;
+		    n3 = n1 + n2;
+		}
 	}
 }
