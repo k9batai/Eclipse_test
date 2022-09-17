2e7527bd43062d4df17385089cbb3d361276ddfd
k9 <k9batai33@gmail.com>
2022-09-17 21:42:42 +0900

    メソッド

---
+++ src/Exercise4.java
@@ -0,0 +1,13 @@
+
+public class Exercise4 {
+	public static void main( String[] args )
+	{
+	    System.out.println( "3の2乗=" + pow2( 3 ) );
+	}
+
+	static int pow2( int n )
+	{
+	    return n * n;
+	}
+
+}
