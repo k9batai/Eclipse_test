3465ed5eaece6d7c852b9d54ec98bcfa7bef3221
k9 <k9batai33@gmail.com>
2022-09-17 21:57:31 +0900

    平均を出すメソッド

--- src/Exercise4.java
+++ src/Exercise4.java
@@ -1,13 +1,14 @@
 
 public class Exercise4 {
 	public static void main( String[] args )
+	
 	{
-	    System.out.println( "3の2乗=" + pow2( 3 ) );
+	System.out.println( "4と9の平均=" + average( 4, 9 ) );
 	}
 
-	static int pow2( int n )
+	static int average( int x, int y )
 	{
-	    return n * n;
+	    return ( x + y ) / 2;
 	}
 
 }
