9788ee0cb4c869e9b9caac3416674ce5f3a5e6b8
k9 <k9batai33@gmail.com>
2022-09-18 21:54:48 +0900

    素数を表示する

--- src/Exercise4.java
+++ src/Exercise4.java
@@ -1,15 +1,20 @@
 public class Exercise4 {
 	public static void main( String[] args )
 	{
-	    for( int i = 1 ; i <= 9 ; i++ )
-	        KukuColumn( i );
+	    for( int n = 10000 ; n < 10100 ; n++ )
+	        if( IsPrimeNumber( n ) )
+	            System.out.print( n + " " );
 	}
 
-	static void KukuColumn( int num )
+	static boolean IsPrimeNumber( int num )
 	{
-	    for( int i = 1 ; i <= 9 ; i++ )
-	        System.out.printf( " %2d", num * i );
+	    if( num <= 3 )
+	        return true;
 
-	    System.out.println();
+	    for( int i = 2 ; i <= ( num / 2 ) ; i++ )
+	        if( ( num % i) == 0 )
+	            return false;
+
+	    return true;
 	}
 }
