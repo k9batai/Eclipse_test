248d503460979b4ef1f310e18051ab6b635b7112
k9 <k9batai33@gmail.com>
2022-09-18 22:31:13 +0900

    クラス

---
+++ src/Exercise5.java
@@ -0,0 +1,25 @@
+class Dog
+{
+    private String Name;
+
+    public void SetName( String nm )
+    {
+        Name = nm;
+    }
+
+    public void ShowProfile()
+    {
+        System.out.println( "名前は、" + Name + "です。" );
+    }
+}
+
+public class Exercise5
+{
+    public static void main( String[] args )
+    {
+        Dog MyDog = new Dog();
+
+        MyDog.SetName( "ぽち" );
+        MyDog.ShowProfile();
+    }
+}
