5dda98a4ad25baccbd39d863191cf8373ce23990
k9 <k9batai33@gmail.com>
2022-09-19 23:12:41 +0900

    継承

---
+++ src/Exercise7.java
@@ -0,0 +1,31 @@
+class Animal
+{
+    public String Name;
+    public int Age;
+
+    public void ShowProfile()
+    {
+        System.out.println( "名前は、" + Name + "、" + Age + "歳です。" );
+    }
+}
+
+class Cat extends Animal
+{
+    public void Sleep()
+    {
+        System.out.println( "スースー" );
+    }
+}
+
+public class Exercise7
+{
+    public static void main( String[] args )
+    {
+        Cat MyCat = new Cat();
+
+        MyCat.Name = "たま";
+        MyCat.Age = 3;
+        MyCat.ShowProfile();
+        MyCat.Sleep();
+    }
+}
