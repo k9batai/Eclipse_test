d9c1953a3d700ade8623108f95b8651a22ae7db4
k9 <k9batai33@gmail.com>
2022-09-24 22:11:41 +0900

    メソッド

--- src/If02.java
+++ src/If02.java
@@ -1,19 +1,11 @@
 
 public class If02 {
 	public static void main(String[] args) {
-		int number = 7;
-			if (number < 5) {
-				System.out.println(number + " は5未満です。 ");
-			}	
-			else if (number < 10) {
-				System.out.println(number + " は5以上、10未満です。 ");
-			}
-			else if (number < 15) {
-				System.out.println(number + " は10以上、15未満です。 ");
-			}
-			else {
-				System.out.println(number + " は15未満ではないです。 ");
-			}
+		hello();
 	}
-
+	
+	public static void hello() {
+		// "Hello World"を、"Hello Java"に書き換えてください
+		System.out.println("Hello Java");
+	}
 }
