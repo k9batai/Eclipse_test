afa9c838ff59ef1ad6a2bacfc8f1554e4393d224
k9 <k9batai33@gmail.com>
2022-09-24 23:30:18 +0900

    メソッド　第2引数

--- src/If02.java
+++ src/If02.java
@@ -1,11 +1,11 @@
 
 public class If02 {
 	public static void main(String[] args) {
-		hello("ジョン");
+		hello("ジョン", 30);
 	}
 	
-	public static void hello(String name) {
+	public static void hello(String name, int age) {
 		// "Hello World"を、"Hello Java"に書き換えてください
-		System.out.println(name);
+		System.out.println(name + "です" + age + "歳です。");
 	}
 }
