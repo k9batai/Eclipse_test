7adc4ddeae375e3ace4c321bc2cfb63b1f3524e8
k9 <k9batai33@gmail.com>
2022-09-25 13:36:57 +0900

    メソッド

--- src/If02.java
+++ src/If02.java
@@ -2,10 +2,15 @@
 public class If02 {
 	public static void main(String[] args) {
 		hello("ジョン", 30);
+		dog("ぽち");
 	}
 	
 	public static void hello(String name, int age) {
 		// "Hello World"を、"Hello Java"に書き換えてください
 		System.out.println(name + "です" + age + "歳です。");
+	}
+	
+	public static void dog(String name) {
+		System.out.println("愛犬の名前は" + name + "です");
 	}
 }
