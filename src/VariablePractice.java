4a2d1cfb7f0f4dc38826a0ce611dbbdd457fb48c
k9 <k9batai33@gmail.com>
2022-09-26 21:31:48 +0900

    練習問題

--- src/VariablePractice.java
+++ src/VariablePractice.java
@@ -49,6 +49,27 @@ public class VariablePractice {
         
         // s2 の値を出力してください。
         System.out.println(s2);
+        
+        
+        
+     // 整数変数の宣言と初期化
+        int value = 12;        // ←値を変更しながら実行
+        System.out.println("value：" + value);
+        // if文の開始
+        if (value % 2 == 0) {
+        	System.out.println("偶数です");	
+        } else {
+        	System.out.println("奇数です");
+        }
+        if (value % 3 == 0) {
+        	System.out.println("3の倍数です");
+        } 
+        if (value % 4 == 0) {
+        	System.out.println("4の倍数です");
+        }
+        if (value % 5 == 0) {
+        	System.out.println("5の倍数です");
+        }
 	}	
 
 }
