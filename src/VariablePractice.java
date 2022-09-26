351bb7841a938dfd37b0af3319647c2ace0a1b75
k9 <k9batai33@gmail.com>
2022-09-26 19:40:39 +0900

    練習問題

---
+++ src/VariablePractice.java
@@ -0,0 +1,18 @@
+
+public class VariablePractice {
+	public static void main(String[] args) {
+		int price;
+		price = 130; 
+		
+		int num = 6;
+		
+		int allNum = price * num;
+		
+		System.out.println(price);
+		
+		System.out.println(num);
+		
+		System.out.println(allNum);
+	}	
+
+}
