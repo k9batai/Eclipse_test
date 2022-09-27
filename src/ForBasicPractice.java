29b82a3052591cf55a3d8cce6fc04cfd06e5fc8c
k9 <k9batai33@gmail.com>
2022-09-27 22:22:37 +0900

    for文

--- src/ForBasicPractice.java
+++ src/ForBasicPractice.java
@@ -8,5 +8,18 @@ public class ForBasicPractice {
     			}
     		}
     	}
+    	
+    	 // 山の段数を表す変数
+        int level = 5;
+        // for文の開始
+        for(int i = 0; i < level; i++) {
+            for(int j = 0; j < level - i - 1; j++) {
+                System.out.print("　");
+            }
+            for(int j = 0; j < (i * 2) + 1; j++) {
+                System.out.print("■");
+            }
+            System.out.println();
+        }
     }
 }
