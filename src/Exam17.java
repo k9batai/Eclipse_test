e332bd80b47f304389d163eebc4ec7a0648815fd
k9 <k9batai33@gmail.com>
2022-09-29 10:18:27 +0900

    消費税の計算

--- src/Exam17.java
+++ src/Exam17.java
@@ -7,6 +7,11 @@ public class Exam17 {
 		
 		System.out.print(number1 + operator + number2 + "=");
 		System.out.println(number1 / (double)number2);
+		
+		int price = 980;
+		int tax_rate = 8;
+		
+		System.out.println( (int)(price + (price * tax_rate * 0.01) ) );
 	}
 
 }
