ce0bfb008983ea6e236332ae8d0a970285976015
k9 <k9batai33@gmail.com>
2022-09-29 09:51:37 +0900

    練習問題

--- src/ClassFirstPractice.java
+++ src/ClassFirstPractice.java
@@ -1,33 +1,30 @@
-public class ClassFirstPractice {
+public class ClassPractice {
     public static void main(String[] args) {
-        // Employeeクラスのオブジェクトを作成
-        Employee employee = new Employee();
-        // Employeeオブジェクトに社員番号と社員名をセット
-        employee.setData(1234, "山田");
-        // Employeeオブジェクトの社員番号を出力
-        System.out.println("社員番号：" + employee.getId());
-        // Employeeオブジェクトの社員名を出力
-        System.out.println("社員名：" + employee.getName());
+
+        // 1匹目のペットオブジェクトの生成
+        Pet pet1 = new Pet();
+
+        // 1匹目のペットデータを設定
+        pet1.setType("犬");
+        pet1.setName("コタロー");
+        pet1.setAge((byte)4);
+        pet1.setGender(false);
+
+        // 2匹目のペットオブジェクトの生成
+        Pet pet2 = new Pet();
+
+        // 2匹目のペットのデータを設定
+        pet2.setType("猫");
+        pet2.setName("ミー");
+        pet2.setAge((byte)2);
+        pet2.setGender(true);
+
+        // 2匹のペットの紹介文を取得し、表示する
+        System.out.println(pet1.getIntroduction());
+        System.out.println(pet2.getIntroduction());
+
     }
 }
 
-//Employeeクラス
-class Employee {
-	// 社員番号
-	private int id;
-	// 社員名
-	private String name;
-	// 社員番号と社員名をオブジェクトにセットする
-	public void setData(int id, String name) {
-	    this.id = id;
-	    this.name = name;
-	}
-	// 社員番号の情報を取得する
-	public int getId() {
-	    return id;
-	}
-	// 社員名の情報を取得する
-	public String getName() {
-	    return name;
-	}
-}
+
+// ここにPetクラスを作成してください。
