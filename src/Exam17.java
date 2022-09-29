f8b3aab49de45c5d9b595f3fd10bca111862154b
k9 <k9batai33@gmail.com>
2022-09-29 10:07:59 +0900

    変数と型と演算

---
+++ src/Exam17.java
@@ -0,0 +1,12 @@
+
+public class Exam17 {
+	public static void main(String[] args) {
+		int number1 = 25;
+		int number2 = 2;
+		String operator = "/";
+		
+		System.out.print(number1 + operator + number2 + "=");
+		System.out.println(number1 / (double)number2);
+	}
+
+}
