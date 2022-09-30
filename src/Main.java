e9161cf067dafe325d70f46b1920c2f40528b71d
k9 <k9batai33@gmail.com>
2022-09-30 19:05:55 +0900

    戻り値

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,27 @@ public class Main {
     public static void main(String[] args) {
     	int addNum = add(5, 2);
     	System.out.println("答えは" + addNum + "です");
+    	
+    	boolean b = divide5(21);
+    	if(b) {
+    		System.out.println("割り切れます。");
+    	} else {
+    		System.out.println("割り切れません。");
+    	}
 
     }
 
     private static int add(int num1, int num2) {
         int addAns = num1 + num2;
         return addAns;
+    }
+    
+    private static boolean divide5(int num1) {
+    	int ans = num1 % 5;
+    	if(ans == 0) {
+    		return true;
+    	}
+    	return false;
     }
 
 }
