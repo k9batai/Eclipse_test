5e9d8276b37102c7f6d505ccbc266f60e645b691
k9 <k9batai33@gmail.com>
2022-09-30 22:22:32 +0900

    オブジェクト指向

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,30 @@
 public class Main {
-
     public static void main(String[] args) {
-    	int addNum = add(5, 2);
-    	System.out.println("答えは" + addNum + "です");
-    	
-    	boolean b = divide5(21);
-    	if(b) {
-    		System.out.println("割り切れます。");
-    	} else {
-    		System.out.println("割り切れません。");
-    	}
-    	
-    	String[] arrays1 = {"a", "b", "c"};
-		System.out.println(getFirstString(arrays1));
- 
-		String[] arrays2 = {};
-		System.out.println(getFirstString(arrays2));
-		
-		int num1 = 2;
-		int num2 = 3;
- 
-		Calculation calc = new Calculation();
-		Calculation value = calc.calcMethod(num1, num2);
- 
-		System.out.println("足し算：" + value.add);
-		System.out.println("掛け算：" + value.mul);
-
+        // Employeeクラスのオブジェクトを作成
+        Employee employee = new Employee();
+        // Employeeオブジェクトに社員番号と社員名をセット
+        employee.setDate(1234, "山田");
+        // Employeeオブジェクトの社員番号を出力
+        System.out.println("社員番号：" + employee.getId());
+        // Employeeオブジェクトの社員名を出力
+        System.out.println("社員名：" + employee.getName());
     }
-
-    private static int add(int num1, int num2) {
-        int addAns = num1 + num2;
-        return addAns;
-    }
-    
-    private static boolean divide5(int num1) {
-    	int ans = num1 % 5;
-    	if(ans == 0) {
-    		return true;
-    	}
-    	return false;
-    }
-    
-    private static String getFirstString(String[] arrays) {
-    	if(arrays == null || arrays.length == 0) {
-    		return null;
-    	}
-    	return arrays[0];
-    }
-
 }
 
-class Calculation {
-	int add;
-	int mul;
- 
-	public Calculation calcMethod(int num1, int num2) {
-		Calculation calc = new Calculation();
-		calc.add = num1 + num2;
-		calc.mul = num1 * num2;
- 
-		return calc;
+class Employee {
+	private int id;
+	private String name;
+	
+	public void setDate(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
 	}
 }
