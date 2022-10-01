5eba1c5fd92bef2652c4c825c93d5623bef2c8e4
k9 <k9batai33@gmail.com>
2022-10-01 21:14:02 +0900

    メソッド

--- src/ClassNormalPractice.java
+++ src/ClassNormalPractice.java
@@ -17,3 +17,46 @@ public class ClassNormalPractice {
         persons[2].compare(persons[1]);
     }
 }
+
+class Person {
+	private String name;
+	private int age;
+	
+	public void setData(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void introduce() {
+		System.out.println("わたしの名前は" + name + "、" + "年齢は" + age + "歳です。");
+	}
+	
+    public void compare(Person person) {
+        if(age > person.getAge()) {
+            System.out.println("わたくし" + name + "は、" +
+                    person.getName() + "さんより" +
+                    (age - person.getAge()) +
+                    "歳年上です。");
+        }
+        else if(age < person.getAge()) {
+            System.out.println("わたくし" + name + "は、" +
+                    person.getName() + "さんより" +
+                    (person.getAge() - age) +
+                    "歳年下です。");
+        }
+        else {
+            System.out.println("わたくし" + name + "は、" +
+                    person.getName() + "さんと同じ年齢です。");
+        }
+    }
+	
+	
+}
