8d8d4bbcac94e519403f0db3af1453f6e57fb8cf
k9 <k9batai33@gmail.com>
2022-10-01 09:04:00 +0900

    メソッド

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,28 @@ public class Main {
         System.out.println("社員番号：" + employee.getId());
         // Employeeオブジェクトの社員名を出力
         System.out.println("社員名：" + employee.getName());
+        
+        // 1匹目のペットオブジェクトの生成
+        Pet pet1 = new Pet();
+        
+        // 1匹目のペットデータを設定
+        pet1.setType("犬");
+        pet1.setName("コタロー");
+        pet1.setAge( (byte)2 );
+        pet1.setGender(true);
+        
+        // 2匹目のペットオブジェクトの生成
+        Pet pet2 = new Pet();
+
+        // 2匹目のペットのデータを設定
+        pet2.setType("猫");
+        pet2.setName("ミー");
+        pet2.setAge((byte)2);
+        pet2.setGender(true);
+
+        // 2匹のペットの紹介文を取得し、表示する
+        System.out.println(pet1.getIntroduction());
+        System.out.println(pet2.getIntroduction());
     }
 }
 
@@ -27,4 +49,8 @@ class Employee {
 	public String getName() {
 		return name;
 	}
+}
+
+class Pet {
+	
 }
