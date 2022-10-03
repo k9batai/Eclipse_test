45b0d3b33a5a1cfa39da40418ca7519e7ec7461f
k9 <k9batai33@gmail.com>
2022-10-03 22:21:24 +0900

    オブジェクト指向　練習問題

--- src/VariablePractice.java
+++ src/VariablePractice.java
@@ -11,6 +11,30 @@ public class VariablePractice {
         // 2台の車の情報を出力
         car1.showInfo();
         car2.showInfo();
+        
+     // ロボット総生産数の表示
+        System.out.println("ロボット総生産数：" + Robot.getTotal());
+        // 5体のロボットオブジェクトを作成
+        // （6体目は保留）
+        Robot[] robots = {
+                new Robot("RX"),
+                new Robot("PZ"),
+                new Robot("SS"),
+                new Robot("FG"),
+                new Robot("VC"),
+                null
+        };
+        // ロボット総生産数の表示
+        System.out.println("ロボット総生産数：" + Robot.getTotal());
+        // 6体目のロボットオブジェクトを作成
+        robots[5] = new Robot("SUPER-Z");
+        // ロボット総生産数の取得
+        System.out.println("ロボット総生産数：" + Robot.getTotal());
+        System.out.println();
+        // 6体のロボットの情報を表示
+        for(int i = 0; i < robots.length; i++) {
+            robots[i].introduce();
+        }
     }
 
 }
