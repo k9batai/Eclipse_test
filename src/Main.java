f8d009480fbb5f70cf6e75137ecd5e5d43748b50
k9 <k9batai33@gmail.com>
2022-10-03 21:15:02 +0900

    オブジェクト指向　練習問題

--- src/Main.java
+++ src/Main.java
@@ -55,3 +55,28 @@ class Chef {
  }
 }
 
+class Square {
+	// 幅
+    private double width;
+    // 高さ
+    private double height;
+ // コンストラクタ
+    public Square(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+ // 情報出力メソッド
+    public void inform() {
+        System.out.println("この四角形の幅は" + width + "、高さは" +
+                    height + "です。");
+    }
+    // 面積算出メソッド
+    public double getArea() {
+        return width * height;
+    }
+    // 幅増加メソッド
+    public void addWidth(double delta) {
+        width += delta;
+    }
+}
+
