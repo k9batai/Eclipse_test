981db28e7c34a2debbcd56e19bc7a95ac8d7632f
k9 <k9batai33@gmail.com>
2022-10-04 22:48:47 +0900

    オブジェクト指向　練習問題

--- src/Main.java
+++ src/Main.java
@@ -1,82 +1,29 @@
-class Main {
-	public static void main(String[] args) {
-        // シェフオブジェクトの生成
-        Chef chef = new Chef();
-        // 食材オブジェクトの生成
-        Egg egg = new Egg();
-        Rice rice = new Rice();
-        Milk milk = new Milk();
-        Cheese cheese = new Cheese();
-        // シェフがフルコースを調理します
-        System.out.println("1品目は" + chef.cook(egg, cheese) + "です");
-        System.out.println("2品目は" + chef.cook(rice, egg) + "です");
-        System.out.println("3品目は" + chef.cook(rice, cheese) + "です");
-        System.out.println("4品目は" + chef.cook(milk, egg) + "です");
-        
-        // Squareクラスのオブジェクトを作成
-        Square square = new Square(4.5, 2.8);
-        // 四角形の情報を出力
-        square.inform();
-        // 面積の表示
-        System.out.println("この四角形の面積は" + square.getArea() + "です。");
-        // 幅を3.0増加させる
-        square.addWidth(3.0);
-        System.out.println();
-        // 四角形の情報を出力
-        square.inform();
-        // 面積の表示
-        System.out.println("この四角形の面積は" + square.getArea() + "です。");
+public class Main {
+    public static void main(String[] args) {
+        // Magazineクラスのオブジェクトを生成
+        Magazine magazine = new Magazine("東京Runner", 650, "上野彩");
+        // 雑誌の内容紹介メソッドの呼び出し
+        magazine.show();
     }
 }
-
-
-//各種食材クラス
-class Egg{}
-class Rice{}
-class Milk{}
-class Cheese{}
-//Chefクラス
-class Chef {
- // 調理メソッド
- public String cook(Egg egg, Cheese cheese) {
-     return "スクランブルエッグ";
- }
- // 調理メソッド
- public String cook(Rice rice, Egg egg) {
-     return "オムライス";
- }
- // 調理メソッド
- public String cook(Rice rice, Cheese cheese) {
-     return "リゾット";
- }
- // 調理メソッド
- public String cook(Milk milk, Egg egg) {
-     return "プディング";
- }
-}
-
-class Square {
-	// 幅
-    private double width;
-    // 高さ
-    private double height;
- // コンストラクタ
-    public Square(double width, double height) {
-        this.width = width;
-        this.height = height;
+// Bookクラス
+class Book {
+    // タイトル
+    private String title;
+    // 価格
+    private int price;
+    // コンストラクタ
+    public Book(String title, int price) {
+        this.title = title;
+        this.price = price;
     }
- // 情報出力メソッド
-    public void inform() {
-        System.out.println("この四角形の幅は" + width + "、高さは" +
-                    height + "です。");
+    // タイトル取得メソッド
+    public String getTitle() {
+        return title;
     }
-    // 面積算出メソッド
-    public double getArea() {
-        return width * height;
-    }
-    // 幅増加メソッド
-    public void addWidth(double delta) {
-        width += delta;
+    // 価格取得メソッド
+    public int getPrice() {
+        return price;
     }
 }
 
