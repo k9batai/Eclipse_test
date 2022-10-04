7832a4dca2eeb7cdeace53d155b1149b33168823
k9 <k9batai33@gmail.com>
2022-10-04 23:23:12 +0900

    スーパークラスの継承について

--- src/Main.java
+++ src/Main.java
@@ -61,3 +61,18 @@ class Book {
      return price;
  }
 }
+
+class Magazine extends Book {
+	private String coverPersonName;
+	
+	public Magazine(String title, int price, String coverPersonName ) {
+		super(title, price);
+		this.coverPersonName = coverPersonName;
+	}
+	
+	public void show() {
+        System.out.println(getTitle() + "絶賛発売中！！");
+        System.out.println("定価" + getPrice() + "円");
+        System.out.println("今回の表紙：" + coverPersonName + "さん");
+    }
+}
