0ff7e2c3f6f3d18dad952e52746cb337ceb24091
k9 <k9batai33@gmail.com>
2022-10-04 23:04:15 +0900

    継承

--- src/Main.java
+++ src/Main.java
@@ -16,3 +16,20 @@ class BaseballPlayer {
     protected double battingAverage;
 }
 
+class Pitcher extends BaseballPlayer {
+	private double era;
+	
+	public Pitcher(String name, int uniformNumber, double battingAverage, double era) {
+		this.name = name;
+        this.uniformNumber = uniformNumber;
+        this.battingAverage = battingAverage;
+        this.era = era;
+	}
+	
+	public void introduce() {
+		System.out.println("選手名：" + name);
+	    System.out.println("背番号：" + uniformNumber);
+	    System.out.println("打率　：" + battingAverage);
+	    System.out.println("防御率：" + era);
+	}
+}
