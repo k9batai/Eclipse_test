c43ef4e0c25e7e10aee9c824d85c6c076b68f1d1
k9 <k9batai33@gmail.com>
2022-10-04 22:51:03 +0900

    オブジェクト指向　練習問題

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        // Magazineクラスのオブジェクトを生成
-        Magazine magazine = new Magazine("東京Runner", 650, "上野彩");
-        // 雑誌の内容紹介メソッドの呼び出し
-        magazine.show();
+        // 投手オブジェクトの生成
+        Pitcher p = new Pitcher("山田", 99, 0.09, 2.13);
+        // 投手の紹介
+        p.introduce();
     }
 }
-// Bookクラス
-class Book {
-    // タイトル
-    private String title;
-    // 価格
-    private int price;
-    // コンストラクタ
-    public Book(String title, int price) {
-        this.title = title;
-        this.price = price;
-    }
-    // タイトル取得メソッド
-    public String getTitle() {
-        return title;
-    }
-    // 価格取得メソッド
-    public int getPrice() {
-        return price;
-    }
+// 野球選手クラス
+class BaseballPlayer {
+    // 選手名
+    protected String name;
+    // 背番号
+    protected int uniformNumber;
+    // 打率
+    protected double battingAverage;
 }
 
