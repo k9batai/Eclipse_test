ca254d26e0dff86c6d64012815780c89b4e86bc4
k9 <k9batai33@gmail.com>
2022-10-05 22:13:48 +0900

    オブジェクト指向　練習問題

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,22 @@ public class Main {
         for(int i = 0; i < animals.length; i++) {
             animals[i].sing();
         }
+        
+     // センターフォワードオブジェクトの生成
+        SoccerPlayer fwd = new CenterForward("田村", 11);
+        // 自己紹介
+        System.out.println(fwd);
+        // ボールを処理
+        fwd.kickBall();
+        fwd.catchBall();
+        System.out.println();
+        // ゴールキーパーオブジェクトの生成
+        SoccerPlayer keeper = new GoalKeeper("川島", 12);
+        // 自己紹介
+        System.out.println(keeper);
+        // ボールを処理
+        keeper.kickBall();
+        keeper.catchBall();
     }
 }
 // 動物クラス
