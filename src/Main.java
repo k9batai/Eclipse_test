cba632ca19e08ca9154478bc64af918c981e8bc6
k9 <k9batai33@gmail.com>
2022-10-05 21:35:53 +0900

    オブジェクト指向　練習問題

--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,17 @@
-public class Main {
+public class AbstractBasicPractice {
     public static void main(String[] args) {
-        // 投手オブジェクトの生成
-        Pitcher p = new Pitcher("山田", 99, 0.09, 2.13);
-        // 投手の紹介
-        p.introduce();
-        
-        
-     // Magazineクラスのオブジェクトを生成
-        Magazine magazine = new Magazine("東京Runner", 650, "上野彩");
-        // 雑誌の内容紹介メソッドの呼び出し
-        magazine.show();
+        // 3匹の動物たちを表すAnimal配列型変数animalsの宣言と
+        // 配列オブジェクトの生成
+        // 動物たち配列変数の各要素に、順番に犬・猫・鳥
+        // オブジェクトを生成・代入
+        // forループを使って動物たち配列変数を順にみていき、
+        // 各動物の歌うsingメソッドの呼び出し
     }
 }
-// 野球選手クラス
-class BaseballPlayer {
-    // 選手名
-    protected String name;
-    // 背番号
-    protected int uniformNumber;
-    // 打率
-    protected double battingAverage;
+// 動物クラス
+abstract class Animal {
+    public abstract void sing();
 }
-
-class Pitcher extends BaseballPlayer {
-	private double era;
-	
-	public Pitcher(String name, int uniformNumber, double battingAverage, double era) {
-		this.name = name;
-        this.uniformNumber = uniformNumber;
-        this.battingAverage = battingAverage;
-        this.era = era;
-	}
-	
-	public void introduce() {
-		System.out.println("選手名：" + name);
-	    System.out.println("背番号：" + uniformNumber);
-	    System.out.println("打率　：" + battingAverage);
-	    System.out.println("防御率：" + era);
-	}
-}
-
-
-//Bookクラス
-class Book {
- // タイトル
- private String title;
- // 価格
- private int price;
- // コンストラクタ
- public Book(String title, int price) {
-     this.title = title;
-     this.price = price;
- }
- // タイトル取得メソッド
- public String getTitle() {
-     return title;
- }
- // 価格取得メソッド
- public int getPrice() {
-     return price;
- }
-}
-
-class Magazine extends Book {
-	private String coverPersonName;
-	
-	public Magazine(String title, int price, String coverPersonName ) {
-		super(title, price);
-		this.coverPersonName = coverPersonName;
-	}
-	
-	public void show() {
-        System.out.println(getTitle() + "絶賛発売中！！");
-        System.out.println("定価" + getPrice() + "円");
-        System.out.println("今回の表紙：" + coverPersonName + "さん");
-    }
-}
+// ここにDogクラスを作成してください
+// ここにCatクラスを作成してください
+// ここにBirdクラスを作成してください
