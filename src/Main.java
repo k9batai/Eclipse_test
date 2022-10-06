8d8201275db7804c6b75ea7410df55202dd82a63
k9 <k9batai33@gmail.com>
2022-10-06 22:33:19 +0900

    メソッドの作成と呼び出し

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main{
     public static void main(String[ ] args){
     	 returnTest(2); // メソッドの呼び出し。問題のない引数
          returnTest(-1);    // メソッドの呼び出し。問題のある引数
+         
+         int times = 10;
+         String msg = "こんにちは、Java";
+         // メソッドの呼び出し
+         printMessage(msg, times);
     }
     
     static void returnTest(int i){
@@ -12,5 +17,11 @@ public class Main{
         }
         System.out.println("\t問題のない引数");
         return;       // return を省略せずに記述
+    }
+    
+    static void printMessage(String str, int i){
+        for(int j = 0; j < i; j++){
+            System.out.println(str);
+        }
     }
 }
