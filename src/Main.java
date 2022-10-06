3612b456ec840c1cedbbfd8651c74b38feddebbb
k9 <k9batai33@gmail.com>
2022-10-06 22:28:48 +0900

    return文

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,16 @@
 public class Main{
     public static void main(String[ ] args){
-        int i = (int)(Math.random( ) * 6) + 1;  // キャストする
-        System.out.println("ランダムに生成した数値は : " + i + "です");
-        
-        String str = "100";
-        int a = Integer.parseInt(str); //文字列の"100"を整数の100に変換
-        int ans = a + 20; //i はstrの表す文字列と同じ意味の数値が入っている
-        System.out.println("足し算の結果 : " + ans);
-        
-        printHello();
-        String msg = "こんにちは、Java";
-        printMessage(msg);  // メソッドの呼び出し
+    	 returnTest(2); // メソッドの呼び出し。問題のない引数
+         returnTest(-1);    // メソッドの呼び出し。問題のある引数
     }
     
-    static void printHello( ){
-        String msg = "メソッドが呼ばれました";
-        System.out.println(msg);
-    }
-    
-    static void printMessage(String greeting){
-        System.out.println("メソッドから出力 : " + greeting);
+    static void returnTest(int i){
+        System.out.println("メソッドが呼ばれました。引数は" + i + "です");
+        if(i < 0){    // まず、引数に問題がないか検査
+            System.out.println("\t引数の値は０以上にしてください");
+            return;       // ここでメソッドを終了
+        }
+        System.out.println("\t問題のない引数");
+        return;       // return を省略せずに記述
     }
 }
