23436ccf0f66d370ed7c84f247630ebc516774ce
k9 <k9batai33@gmail.com>
2022-10-06 22:59:37 +0900

    メソッドの作成と呼び出し

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,34 @@
 public class Main{
-    public static void main(String[ ] args){
-    	 returnTest(2); // メソッドの呼び出し。問題のない引数
-         returnTest(-1);    // メソッドの呼び出し。問題のある引数
-         
-         int times = 10;
-         String msg = "こんにちは、Java";
-         // メソッドの呼び出し
-         printMessage(msg, times);
-         
-         int i = 10;
-         executeCalcSigma(i);    // メソッドを呼び出し 
-         executeCalcSigma(30);  // 別の引数でメソッドを呼び出し
-         
-         int ret, data = 10;
-         ret = max(data, 30);
-         System.out.println("戻り値は : " + ret);
-         
-         double result, value = 12.3;
-         result = max(value, 98.7);
-         System.out.println("戻り値は : " + result);
+	public static void main(String[ ] args){
+        // 戻り値、引数リストのあるメソッドの呼び出し
+        String returnString = mojiretuRenketu("Hello, Java.", 3);
+        System.out.println("mojiretuRenketuの結果 : " + returnString);
+        // 引数があり、戻り値のないメソッドの呼び出し
+        printMessage("こんにちは、Javaの勉強は進んでいますか？");
+        // 引数がなく、戻り値があるメソッドの呼び出し
+        int returnVal = getCount( );
+        System.out.println("getCountの結果" + returnVal);
+        // 引数も戻り値もないメソッドの呼び出し
+        printHello( );
     }
-    
-    static void returnTest(int i){
-        System.out.println("メソッドが呼ばれました。引数は" + i + "です");
-        if(i < 0){    // まず、引数に問題がないか検査
-            System.out.println("\t引数の値は０以上にしてください");
-            return;       // ここでメソッドを終了
-        }
-        System.out.println("\t問題のない引数");
-        return;       // return を省略せずに記述
-    }
-    
-    static void printMessage(String str, int i){
+    static String mojiretuRenketu(String str, int i){
+        String ret = "";
         for(int j = 0; j < i; j++){
-            System.out.println(str);
+            ret += str;
         }
+        return ret;
     }
-    
-    static void executeCalcSigma(int i){
-        int ans = 0;
-        for(int j = i; j != 0; j--){
-            ans += j;
-        }
-        System.out.println("答えは : " + ans +"です");
+    static void printMessage(String str){
+        System.out.println("メッセージは「" + str + "」です");
     }
-    
-    static int max(int num1, int num2){
-        if(num1 > num2){
-            return num1;
+    static int getCount( ){
+        int i = 0;
+        for(int j = 0; j < 10; j++){
+            i += j;
         }
-        return num2;
+        return i;
     }
-    
-    static double max(double num1, double num2){
-        if(num1 > num2){
-            return num1;
-        }
-        return num2;
-    }    
+    static void printHello( ){
+        System.out.println("Hello");
+    }
 }
