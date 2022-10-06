5e67558f83e8bd2c1153a09825ac5ad1ccc1782a
k9 <k9batai33@gmail.com>
2022-10-06 22:12:26 +0900

    nteger.parseInt メソッド

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main{
     public static void main(String[ ] args){
         int i = (int)(Math.random( ) * 6) + 1;  // キャストする
         System.out.println("ランダムに生成した数値は : " + i + "です");
+        
+        String str = "100";
+        int a = Integer.parseInt(str); //文字列の"100"を整数の100に変換
+        int ans = a + 20; //i はstrの表す文字列と同じ意味の数値が入っている
+        System.out.println("足し算の結果 : " + ans);
     }
 }
