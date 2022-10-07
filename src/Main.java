f38db15ac86aa3a404b980e978974e18a490b51a
k9 <k9batai33@gmail.com>
2022-10-07 23:23:31 +0900

    フィールド

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 public class Main{
-	public static void main(String[ ] args){
-		 String str1 = "hello";
-	        //そのまま表示してみる
-	        System.out.println(str1);
-	        //文字列を大文字に変換する機能
-	        String str2 = str1.toUpperCase( );
-	        System.out.println(str2);
-	        //文字列の中の'l'の文字をすべて'x'に変換する機能
-	        String str3 = str1.replace('l', 'x');
-	        System.out.println(str3);
-	        //文字列の長さを調べる機能
-	        int size = str1.length( );
-	        System.out.println("文字列の長さは" + size + "です");
+	public static void main(String args[ ]) {
+        Human taro = new Human( );
+        taro.name = "太郎";            //taroの名前
+        taro.age = 30;                 //taroの年齢
+        taro.height = 175.5;           //taroの身長
+        System.out.println(taro.name);
+        System.out.println(taro.age);
+        System.out.println(taro.height);
     }
-}   
+}  
+
+class Human {
+    String name;        //フィールド
+    int age;           //フィールド
+    double height;     //フィールド
+}
