88561f9375151632c8867e23377c1f617fdf5942
k9 <k9batai33@gmail.com>
2022-10-08 23:18:01 +0900

    カプセル化

--- src/VariablePractice.java
+++ src/VariablePractice.java
@@ -1,38 +1,47 @@
 public class VariablePractice {
 	public static void main(String[] args) {
-		System.out.println("一回目：引数1つで呼ぶ");
-        variableArgs("A");
-        System.out.println("二回目：引数2つで呼ぶ");
-        variableArgs("A","B");
-        System.out.println("三回目：引数3つで呼ぶ");
-        variableArgs("A","B","C");
-        
-        variableArgs2(1, "Apple");
+        int MotherLanguage_Score;
+        int English_Score;
+        int Mathematics_Score;
+        MyScore tanaka = new MyScore();
+        tanaka.setMotherLanguage_Score(80);
+        System.out.println("田中さんの国語の成績を登録しました");
+        MotherLanguage_Score = tanaka.getMotherLanguage_Score();
+        System.out.println("田中さんの国語の成績は" + MotherLanguage_Score + "点です" );
         System.out.println();
-        variableArgs2(2, "Apple","Banana");
+        tanaka.setEnglish_Score(100);
+        System.out.println("田中さんの英語の成績を登録しました");
+        English_Score = tanaka.getEnglish_Score();
+        System.out.println("田中さんの英語の成績は" + English_Score + "点です" );
         System.out.println();
-        variableArgs2(3, "Apple","Banana","Orange");
-        System.out.println();
+        tanaka.setMathematics_Score(60);
+        System.out.println("田中さんの数学の成績を登録しました");
+        Mathematics_Score = tanaka.getMathematics_Score();
+        System.out.println("田中さんの数学の成績は" + Mathematics_Score + "点です" );
     }
-	
-	 //可変長引数をもつメソッドの定義
-    public static void variableArgs(String... str) {
-        for(String s : str) {
-            System.out.println(s);
-        }
-    }
-    
-    public static void variableArgs2(int num, String... str){
-        System.out.println("Stringの引数の数：" + num + "個" );
-        for(String s : str){
-            System.out.println(s);
-        }
-    }
-    
-  //明確に引数を指定したメソッドの定義
-    public static void variableArgs2(int num, String str){
-        System.out.println("引数を明確に定義したメソッドがよびだされました");
-    }
+}
 
+	class MyScore{
+    private int MotherLanguage_Score;  //国語のテストの点数
+    private int English_Score;         //英語のテストの点数
+    private int Mathematics_Score;     //数学のテストの点数
+    public void setMotherLanguage_Score(int mother_s){
+        MotherLanguage_Score = mother_s;
+    }
+    public void setEnglish_Score(int en_s){
+        English_Score =  en_s;
+    }
+    public void setMathematics_Score(int math_s){
+        Mathematics_Score = math_s;
+    }
+    public int getEnglish_Score(){
+        return English_Score;
+    }
+    public int getMotherLanguage_Score(){
+        return MotherLanguage_Score;
+    }
+    public int getMathematics_Score(){
+        return Mathematics_Score;
+    }
 }
 
