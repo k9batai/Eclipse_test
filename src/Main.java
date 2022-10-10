c91e46458c23cd31649ea53db170d115d6cc111c
k9 <k9batai33@gmail.com>
2022-10-10 21:57:50 +0900

    参照型変数

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
 public class Main {
 	 public static void main(String[ ] args){
-	        SomeObject obj1 = new SomeObject( );
-	        System.out.println(obj1);
+	        //基本データ型のデータを宣言
+	        int a = 10;
+	        //オブジェクトを宣言
+	        MyObject obj = new MyObject( );
+	        //それぞれの変数を渡す
+	        foo(a, obj);
+	        System.out.println(a);
+	        System.out.println(obj.x);
+	    }
+	    public static void foo(int a, MyObject obj){
+	        //基本データ型のデータを変更
+	        a++;
+	        //オブジェクト内のデータを変更
+	        obj.x++;
 	    }
 }  
 
-class SomeObject {
+class MyObject {
+    public int x = 20;
 }
