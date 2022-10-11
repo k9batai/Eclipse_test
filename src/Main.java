cdc45aec24f2b2f6924191accbf97b282b2051b2
k9 <k9batai33@gmail.com>
2022-10-11 22:44:10 +0900

    配列

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,11 @@
 public class Main {
-	 public static void main(String[ ] args){
-	        //基本データ型のデータを宣言
-	        int a = 10;
-	        //オブジェクトを宣言
-	        MyObject obj = new MyObject( );
-	        //それぞれの変数を渡す
-	        foo(a, obj);
-	        System.out.println(a);
-	        System.out.println(obj.x);
-	    }
-	    public static void foo(int a, MyObject obj){
-	        //基本データ型のデータを変更
-	        a++;
-	        //オブジェクト内のデータを変更
-	        obj.x++;
-	    }
+	public static void main(String[ ] args){
+        int[ ] a = new int[3];
+        a[0] = 9;
+        a[1] = 4;
+        a[2] = 5;
+        for(int i = 0; i < 3; i++)
+            System.out.println(a[i]);
+    }
 }  
 
-class MyObject {
-    public int x = 20;
-}
