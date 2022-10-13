bca6ea89b6a362ec24e9e93b2fa5713df848fcf6
k9 <k9batai33@gmail.com>
2022-10-13 22:47:27 +0900

    メソッド

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,43 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class Exercise1
+public class Exercise1 {
+public static void main( String[] args )
 {
-    public static void main( String[] args ) throws IOException
-    {
-        BufferedReader br = new BufferedReader(
-                                new InputStreamReader( System.in ) );
+    System.out.println( "3の2乗=" + pow2( 3 ) );
+}
 
-        String  s = br.readLine();
-        System.out.println( s );
-        
-        int x = Integer.parseInt( br.readLine() );
-        System.out.println( x );
-        
-        x = Integer.parseInt( br.readLine() );
 
-        System.out.println( x );
-        System.out.println( x * x );
-        System.out.println( x * x * x );
-        
-        x = Integer.parseInt( br.readLine() );
-        int y = Integer.parseInt( br.readLine() );
-
-        System.out.println("和 " + ( x + y ) );
-        System.out.println("差 " + ( x - y ) );
-        System.out.println("積 " + ( x * y ) );
-        System.out.println("商 " + ( x / y ) );
-        System.out.println("余り " + ( x % y ) );
-        
-        x = Integer.parseInt( br.readLine() );
-        y = Integer.parseInt( br.readLine() );
-
-        System.out.println( "平均値=" + ( ( x + y ) / 2 ) );
-        
-        System.out.println( "あなたの年齢は？" );
-        int age = Integer.parseInt( br.readLine() );
-
-        System.out.println( "生まれてから今まで、およそ " + ( age * 365 ) + "日です" );
-    }
+static int pow2( int n )
+{
+    return n * n;
+}
 }
