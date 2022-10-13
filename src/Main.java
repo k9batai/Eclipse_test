0e503a5247b9df68b800d3d7d4070cc67a43e09c
k9 <k9batai33@gmail.com>
2022-10-13 22:27:56 +0900

    継承

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,59 @@
 public class Main {
 	public static void main(String[ ] args){String[ ] a = new String[3];
-	    a[0] = "鈴木 隆弘";
-	    a[1] = "加藤 博一";
-	    a[2] = "高木 豊";
-	    for(int i = 0; i < 3; i++)
-	        System.out.println(a[i]);
+		Salaryman taro = new Salaryman("太郎", 30, 175.5, 30, 60);
+		//Humanクラスのメソッド
+		taro.selfIntroduce( );
+		//Salarymanクラスのメソッド
+		int income = taro.getIncome( );
+		System.out.println("収入は" + income + "です");
     }
 }  
 
+class Human{
+    private String name;
+    private int age;
+    private double height;
+    public Human( ){
+        System.out.println("引数なしコンストラクタが呼ばれました！");
+    }
+    public Human(String n, int a, double h){
+        this( );
+        System.out.println("引数ありコンストラクタが呼ばれました！");
+        name = n;
+        age = a;
+        height = h;
+    }
+    public void setName(String n){
+        name = n;
+    }
+    public void setAge(int a){
+        age = a;
+    }
+    public void setHeight(double h){
+        height = h;
+    }
+    public void selfIntroduce( ){
+        System.out.println("私の名前は" + name + "です");
+        System.out.println("私の年齢は" + age + "です");
+        System.out.println("私の身長は" + height + "です");
+    }
+}
+
+class Salaryman extends Human{
+    private int salary;
+    private int bonus;
+    public Salaryman( ){
+        System.out.println("Salarymanクラスの引数なしコンストラクタが呼ばれました！");
+    }
+    public Salaryman(String n, int a, double h, int s, int b){
+        System.out.println("Salarymanクラスの引数ありコンストラクタが呼ばれました！");
+            setName(n);
+            setAge(a);
+            setHeight(h);
+            salary = s;
+            bonus = b;
+        }
+    public int getIncome( ){
+        return salary * 12 + bonus * 2;
+    }
+}
