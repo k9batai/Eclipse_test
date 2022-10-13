0cef10b6fd378861a032b3e46fe84401023a7ace
k9 <k9batai33@gmail.com>
2022-10-13 23:06:12 +0900

    メソッド

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -2,10 +2,16 @@ public class Exercise1 {
 	public static void main( String[] args )
     {
         Dog2 MyDog = new Dog2();
+        Dog2 MyDog2 = new Dog2();
 
         MyDog.SetName( "ぽち" );
         MyDog.SetAge( 3 );
+
+        MyDog2.SetName( "たま" );
+        MyDog2.SetAge( 6 );
+        
         MyDog.ShowProfile();
+        MyDog2.ShowProfile();
     }
 }
 
