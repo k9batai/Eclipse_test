9969eca4025a4cfcb4c4eea076c959a19f7557c1
k9 <k9batai33@gmail.com>
2022-10-13 22:52:32 +0900

    メソッド

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,12 +1,15 @@
 public class Exercise1 {
-public static void main( String[] args )
-{
-    System.out.println( "3の2乗=" + pow2( 3 ) );
-}
+	public static void main( String[] args ) {
+	    System.out.println( "3の2乗=" + pow2( 3 ) );
+	    System.out.println( "4と9の平均=" + average( 4, 9 ) );
+	}
 
 
-static int pow2( int n )
-{
-    return n * n;
-}
+	static int pow2( int n ) {
+	    return n * n;
+	}
+	
+	static int average(int x, int y) {
+		return (x + y) / 2;
+	}
 }
